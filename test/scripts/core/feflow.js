4181b8c6c62156f1c689c5dd84ecbb0231aaa5ff
cpselvis <cpselvis@gmail.com>
2017-09-29 13:05:50 +0800

    test: call function test case

--- test/scripts/core/feflow.js
+++ test/scripts/core/feflow.js
@@ -14,11 +14,36 @@ describe('Feflow', () => {
   const version = require('../../../package.json').version;
   const base = pathFn.join(osenv.home(), './.feflow');
 
+
+  feflow.cmd.register('test', args => args);
+
   it('constructor', () => {
     feflow.version.should.eql(version);
     feflow.baseDir.should.eql(base + sep);
     feflow.pkgPath.should.eql(pathFn.join(base, 'package.json'));
     feflow.pluginDir.should.eql(pathFn.join(base, 'node_modules') + sep);
+  });
+
+  it('call() - no callback and args is not a function', () => feflow.call('test', {foo: 'bar'}).then(data => {
+    data.should.eql({foo: 'bar'});
+  }));g
+
+  it('call() - callback without args', callback => {
+    feflow.call('test', (err, data) => {
+      should.not.exist(err);
+      data.should.eql({});
+
+      callback();
+    });
+  });
+
+  it('call() - callback with args', callback => {
+    feflow.call('test', {foo: 'bar'}, (err, data) => {
+      should.not.exist(err);
+      data.should.eql({foo: 'bar'});
+
+      callback();
+    });
   });
 
   it('call() - command not registered', () => {
