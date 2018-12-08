680955086f8c7e4d809863e256c7c08b8d0438fa
lewischeng <lewischeng@tencent.com>
2018-12-08 22:34:20 +0800

    chore: 接入ivweb的eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,15 @@
 module.exports = {
     "env": {
         "es6": true,
+        "browser": true,
         "node": true
     },
-    "extends": "eslint:recommended",
     "parserOptions": {
-        "sourceType": "module"
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
     },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": ["eslint:recommended", "ivweb"]
 };
