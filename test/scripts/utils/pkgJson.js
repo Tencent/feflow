6e16e91c90be95382537c7cfab3a0d5f1be75692
Stevenzhai <zzwdzsw@sina.cn>
2019-01-13 23:47:59 +0800

    chore: add test code

--- test/scripts/utils/pkgJson.js
+++ test/scripts/utils/pkgJson.js
@@ -1,13 +1,21 @@
 const should = require('chai').should();
 const expect = require('chai').expect;
-const pkgJson = require('../../../lib/utils/pkgJson')
-
+const pkgJson = require('../../../lib/utils/pkgJson');
 
 describe('pkgJson', () => {
-  pkgJson('feflow-cli', 'latest', '').then(json => {
-    const version = pkg.version;
-    it('check get version', () => {
+  it('check get version', (done) => {
+    pkgJson('feflow-cli', 'latest', 'http://registry.npmjs.org').then(json => {
+      done();
+      const version = json.version;
       expect(version).to.exist;
     })
   })
+
+  it('check get version error', () => {
+    pkgJson('feflow-cli', 'latest', '').then(json => {
+      const err = json.err;
+      expect(err).to.exist;
+    })
+  })
+
 })
