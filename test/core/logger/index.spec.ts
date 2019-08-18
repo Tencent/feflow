83f056a7da66ec190f00e10b511bc5f4ba379c22
cpselvis <cpselvis@gmail.com>
2019-08-18 16:24:11 +0800

    test(logger): add logger test case

---
+++ test/core/logger/index.spec.ts
@@ -0,0 +1,70 @@
+import chai from 'chai';
+import logger from '../../../src/core/logger';
+const should = chai.should();
+
+const captureStream = (stream: any) => {
+  const oldWrite = stream.write;
+  let buf = '';
+  stream.write = function(chunk: any, encoding: any, callback: any) {
+    buf += chunk.toString();
+    oldWrite.apply(stream, arguments);
+  };
+
+  return {
+    unhook: () => {
+      stream.write = oldWrite;
+    },
+    captured: () => {
+      return buf;
+    }
+  };
+};
+
+describe('@feflow/core - Logger system', () => {
+  let hook: any;
+
+  beforeEach(() => {
+    hook = captureStream(process.stderr);
+  });
+
+  afterEach(() => {
+    hook.unhook();
+  });
+
+  it('test debug and silent', () => {
+    const log = logger({
+      debug: true,
+      silent: true
+    });
+    log.debug('hello feflow');
+  });
+
+  it('test no debug and silent', () => {
+    const log = logger({})
+    log.info('hello feflow')
+  });
+
+  it('test debug', () => {
+    const log = logger({
+      debug: true,
+      silent: false
+    });
+    log.debug('hello feflow');
+  });
+
+  it('test warn', () => {
+    const log = logger({
+      debug: true,
+      silent: false
+    });
+    log.warn('hello feflow');
+  });
+
+  it('test error', () => {
+    const log = logger({
+      debug: true,
+      silent: false
+    });
+    log.error('hello feflow');
+  });
+});
