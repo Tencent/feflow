fbf51c27431b58ad60161e6b767a203e51663cdb
fXy-during <fxy1044667399@163.com>
2019-11-20 10:20:10 +0800

    refactor: remove useless branch and code

--- packages/feflow-report/src/api/config.ts
+++ packages/feflow-report/src/api/config.ts
@@ -1,3 +1,4 @@
 export default {
-  REPORT_URL: 'http://193.112.196.252/api/v1/report/command'
+  REPORT_URL: 'http://193.112.196.252/api/v1/report/command',
+  REPORT_PROXY: 'http://127.0.0.1:12639'
 };
