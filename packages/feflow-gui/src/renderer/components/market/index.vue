9646799676510a38fe0769722e3eb3d55231b676
bethonxyfu <bethonxyfu@tencent.com>
2020-04-12 20:32:51 +0800

    feat(market): 插件市场UI

---
+++ packages/feflow-gui/src/renderer/components/market/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <main>
+      <side-bar></side-bar>
+      <section class="market-wrapper">
+        <div class="market-title">插件市场</div>
+        <el-row :gutter="20">
+          <el-col :span="8" v-for="(o, index) in 12" :key="o">
+            <div @click="handleJump(index)" class="box-card">
+              <el-card :class="getRandomBgColor(index)">
+                <h2 class="plugin-title">Feflow-plugin-check</h2>
+                <p class="plugin-text">鬼吹灯专业土木工程</p>
+                <el-button>安装</el-button>
+              </el-card>
+            </div>
+          </el-col>
+        </el-row>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import SideBar from '../SideBar'
+
+export default {
+  name: 'market-page',
+  // components: {  },
+  components: { SideBar },
+  data() {
+    return {
+      activeName: 'create'
+    }
+  },
+  methods: {
+    handleJump(id) {
+      //   直接调用$router.push 实现携带参数的跳转
+      this.$router.push({
+        path: `/market-info/${id}`
+      })
+    },
+    getRandomBgColor(index) {
+      const randomInde = Math.floor(Math.random() * 4) + 1
+      return `el-bg-color-${randomInde}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  display: flex;
+}
+.market-wrapper {
+  height: 550px;
+  overflow: scroll;
+  box-sizing: border-box;
+  width: 100%;
+  padding-left: 50px;
+  padding-top: 45px;
+  margin-top: 4px;
+  padding-bottom: 24px;
+}
+.market-title {
+  padding: 12px 0px;
+  font-size: 26px;
+  /* font-weight: bold; */
+}
+.plugin-title {
+  color: #fff;
+  font-size: 18px;
+}
+.box-card {
+  cursor: pointer;
+}
+.plugin-text {
+  min-height: 56px;
+  color: rgba(255, 255, 255, 05);
+}
+</style>
+
+<style lang="less">
+.el-col {
+  min-width: 236px;
+  margin-top: 12px;
+}
+.market-wrapper {
+  .el-card {
+    border-radius: 6px;
+  }
+
+  .el-bg-color-1 {
+    background-color: #40a9ff;
+  }
+  .el-bg-color-2 {
+    background-color: #d3adf7;
+  }
+  .el-bg-color-3 {
+    background-color: #ffadd2;
+  }
+  .el-bg-color-4 {
+    background-color: #08979c;
+  }
+  .el-button {
+    // background-color: #fff;
+    color: rgb(64, 158, 255);
+    border-width: 0;
+  }
+}
+</style>
+
---
+++ packages/feflow-gui/src/renderer/components/market/info.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <main>
+      <side-bar></side-bar>
+      <section class="market-info-wrapper">
+        <div class="market-info_box">
+          <div class="market-info_meta">
+            <div class="market-info_meta_title">Feflow-plugin-check</div>
+            <p
+              class="market-info_meta_description"
+            >不知道本地全局安装过哪些npm包时，可以输入以下指令进行查看npm ... info得到所有的svn信息，然后提取版本号异步获取： node-cmd包信息会 ...</p>
+            <div class="market-info_meta_more">
+              <span>当前版本: 1.2.0</span>
+              <span>发布时间: 2020-03-02</span>
+              <span>发布者: Mike</span>
+            </div>
+          </div>
+          <div class="market-info_action">
+            <el-button type="primary">安装</el-button>
+          </div>
+        </div>
+
+        <div class="market-info_readme">this is plugin description</div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import SideBar from '../SideBar'
+
+export default {
+  name: 'market-info',
+  // components: {  },
+  components: { SideBar },
+  data() {
+    return {
+      activeName: 'create',
+      pluginId: null
+    }
+  },
+  created() {
+    const id = this.$route.params.id
+    this.pluginId = id || 0
+  },
+  methods: {
+    handleJump(id) {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+main {
+  display: flex;
+}
+.market-info-wrapper {
+  height: 550px;
+  overflow: scroll;
+  box-sizing: border-box;
+  width: 100%;
+  padding-left: 50px;
+  padding-top: 45px;
+  margin-top: 4px;
+  padding-bottom: 24px;
+}
+.market-info {
+  &_box {
+    display: flex;
+  }
+  &_meta {
+    flex-basis: 74%;
+    &_title {
+      color: #000;
+      font-size: 22px;
+      font-weight: bold;
+    }
+    &_description {
+      padding: 12px 0;
+      max-height: 100px;
+      color: #000;
+    }
+    &_more {
+      display: flex;
+      justify-content: space-between;
+      max-width: 80%;
+      color: grey;
+    }
+  }
+  &_action {
+    flex: 1 1;
+    text-align: center;
+    padding-top: 16px;
+  }
+  &_readme {
+    width: 88%;
+    margin-top: 24px;
+    background-color: #efefef;
+    border-radius: 6px;
+    padding: 16px;
+    min-height: 299px;
+  }
+}
+</style>
+
+<style>
+</style>
+
