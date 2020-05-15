24d07e4d21939ea13fcbb503cebf7ca81035f8d6
karltao <karltao@tencent.com>
2020-05-15 18:02:17 +0800

    fix: fix message

--- packages/feflow-cli/src/core/devkit/loadDevkits.ts
+++ packages/feflow-cli/src/core/devkit/loadDevkits.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import Config from './config';
 import getCommandLine from "./commandOptions";
+import { FEFLOW_ROOT } from '../../shared/constant';
 
 const registerDevkitCommand = (command: any, commandConfig: any, directoryPath: any, ctx: any) => {
   const builder = commandConfig.builder;
@@ -46,11 +47,11 @@ export default function loadDevkits(ctx: any): Promise<void> {
           registerDevkitCommand(command, commandConfig, directoryPath, ctx);
         }
       } else {
-        if (path.basename(directoryPath) === '.fef') {
+        if (path.basename(directoryPath) === FEFLOW_ROOT) {
           ctx.logger.debug('Run commands in .fef root will not work.');
         } else {
           ctx.logger.error(
-            `A config file .feflowrc(.js|.yaml|.yml) was detected in ${directoryPath}, but lost required property 'commands' in field 'devkit'. Please check your config file or just delete it.`,
+            `A config file .feflowrc(.js|.yaml|.yml|.json) was detected in ${directoryPath}, but lost required property 'commands' in field 'devkit'. Please check your config file or just delete it.`,
           );
         }
       }
