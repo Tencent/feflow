644dc2baf63ba8d79e7866844f6f8ed84dec577b
fXy-during <fxy1044667399@163.com>
2020-05-21 22:03:26 +0800

    feat(report): double report

---
+++ packages/feflow-report/src/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Namespace for collection of "before" hooks
+ */
+export const HOOK_TYPE_BEFORE = 'before';
+
+/**
+ * Namespace for collection of "after" hooks
+ */
+export const HOOK_TYPE_AFTER = 'after';
+
+// export const REPORT_URL = 'http://193.112.196.252/api/v1/report/command';
+export const REPORT_URL = 'http://127.0.0.1:7001/api/v1/report/command';
+
+export const REPORT_PROXY = 'http://127.0.0.1:12639';
