d801a4f33d50d77802751403d8602fa8d91525c5
fXy-during <fxy1044667399@163.com>
2020-05-28 21:52:38 +0800

    feat: command source

--- packages/feflow-report/src/constants.ts
+++ packages/feflow-report/src/constants.ts
@@ -8,8 +8,13 @@ export const HOOK_TYPE_BEFORE = 'before';
  */
 export const HOOK_TYPE_AFTER = 'after';
 
-const BASIC_URL = "http://report.feflowjs.com";
+const BASIC_URL = 'http://api.feflowjs.com';
 
 export const REPORT_URL = `${BASIC_URL}/api/v1/report/command`;
 
 export const REPORT_PROXY = 'http://127.0.0.1:12639';
+
+export const REPORT_STATUS = {
+  START: 0,
+  COMPLETED: 1,
+};
