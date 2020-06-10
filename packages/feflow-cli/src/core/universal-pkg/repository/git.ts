79c8e20a80a519da714b49c2e4bc2804bf4b2abe
blurooochen <blurooochen@tencent.com>
2020-06-10 10:35:22 +0800

    fix: fetch tags fail

--- packages/feflow-cli/src/core/universal-pkg/repository/git.ts
+++ packages/feflow-cli/src/core/universal-pkg/repository/git.ts
@@ -61,9 +61,7 @@ export async function getCurrentTag(
 
 export function checkoutVersion(repoPath: string, version: string) {
   const command = 'git';
-  spawn.sync(command, ['-C', repoPath, 'checkout', '.'], { stdio: 'ignore' });
-  spawn.sync(command, ['-C', repoPath, 'checkout', 'master'], { stdio: 'ignore' });
-  spawn.sync(command, ['-C', repoPath, 'pull'], { stdio: 'ignore' });
+  spawn.sync(command, ['-C', repoPath, 'fetch', '--tags'], { stdio: 'ignore' });
   const checkArgs = ['-C', repoPath, 'checkout', version];
   return spawn.sync(command, checkArgs, { stdio: 'ignore' });
 }
