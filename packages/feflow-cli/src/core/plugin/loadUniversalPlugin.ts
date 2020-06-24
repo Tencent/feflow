ea07de33cc6edc46a294d117731f8794d8d643de
blurooo <blurooochen@tencent.com>
2020-06-24 11:26:43 +0800

    Fix arg pass (#278)
    * fix: args bypass shell
    
    * fix: single quotes overlap

--- packages/feflow-cli/src/core/plugin/loadUniversalPlugin.ts
+++ packages/feflow-cli/src/core/plugin/loadUniversalPlugin.ts
@@ -80,6 +80,11 @@ async function execPlugin(
       return false;
     }
     return true;
+  }).map(arg => {
+    if (!/^'.*'$/.test(arg)) {
+      return `'${arg}'`;
+    }
+    return arg;
   });
   try {
     plugin.command.run(...args);
