5b60a28cec15191c995d36c37f0e5bd71f9341c7
Sourceliu <q810681653@icloud.com>
2020-08-14 15:03:44 +0800

    fix: 上报校验

--- packages/feflow-cli/src/core/logger/report.ts
+++ packages/feflow-cli/src/core/logger/report.ts
@@ -1,10 +1,28 @@
 const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
+const os = require('os');
 const pkg = require('../../../package.json');
 const PLUGE_NAME = 'feflow-' + pkg.name.split('/').pop();
 
 const LOGGER_LOG_PATH = path.join(__dirname, '../../../logger.log');
+const KEYS_FILE = path.join(__dirname, '../../../.keys');
+const USER_NAME  = os.hostname().split("-")[0];
+const NOW_TIME = new Date().getTime();
+let KYE_FILE = {};
+(async ()=>{
+  try{
+    KYE_FILE = JSON.parse(fs.readFileSync(KEYS_FILE, 'utf-8'));
+  }catch(err){}
+  if(!KYE_FILE['time'] || NOW_TIME - KYE_FILE['time'] > 5184e6){
+    let {data:{result}} = await axios.get(`http://test.feflowjs.com/api/v1/summary/getskey?rtx=${USER_NAME}`);
+    KYE_FILE = {
+      time:NOW_TIME,
+      skey:result.skey
+    }
+    fs.writeFileSync(KEYS_FILE,JSON.stringify(KYE_FILE),'utf-8')
+  }
+})()
 class loggerReport {
   constructor() {}
   init(data: Array<Object> = [], report?: boolean) {
@@ -20,9 +38,13 @@ class loggerReport {
       }
       if (writeData.length && (writeData.length > 20 || report)) {
         //5s 或 数量大于20上报
-        const response = await axios.post('http://feflowjs.com/api/v1/log/save', {
+        const response = await axios.post('http://test.feflowjs.com/api/v1/log/save', {
           plugin: PLUGE_NAME,
           data: JSON.stringify(writeData)
+        },{
+          headers:{
+            skey:KYE_FILE['skey']
+          }
         });
         if(response.status===204){
             writeData = [];
