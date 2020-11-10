07d5350093049cfc2fa59ac089f7da9672237f51
blurooochen <blurooochen@tencent.com>
2020-11-10 16:35:31 +0800

    优化args逻辑

--- packages/feflow-cli/src/shared/args.ts
+++ packages/feflow-cli/src/shared/args.ts
@@ -1,17 +1,21 @@
 import os from 'os';
 
-const OS_WIN = "win32";
+const OS_WIN = 'win32';
 
 // 对命令字符串进行shell转义
-export function escape(arg: string): string {
-    // windows保持原样
-    if (os.platform() === OS_WIN) {
-        return arg;
-    }
-    if (/[^A-Za-z0-9_\/:=-]/.test(arg)) {
-        arg = "'" + arg.replace(/'/g, "'\\''") + "'";
-        arg = arg.replace(/^(?:'')+/g, '')  // 消除重复的单引号
-                .replace(/\\'''/g, "\\'"); // 如果有两个转义单引号，则删除非转义单引号
-    }
+export function escape(arg = ''): string {
+  // windows保持原样
+  if (os.platform() === OS_WIN) {
     return arg;
+  }
+  let newArg = arg;
+  if (newArg.startsWith('\'') && newArg.endsWith('\'')) {
+    newArg = newArg.substring(1, newArg.length - 1);
+  }
+  if (/[^A-Za-z0-9_/:=-]/.test(newArg)) {
+    newArg = '\'' + newArg.replace(/'/g, '\'\\\'\'') + '\'';
+    newArg = newArg.replace(/^(?:'')+/g, '')  // 消除重复的单引号
+      .replace(/\\'''/g, '\\\''); // 如果有两个转义单引号，则删除非转义单引号
+  }
+  return newArg;
 }
