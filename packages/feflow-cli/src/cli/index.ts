c03debe17fce160efa64cc6e73cc9913f75ca2ab
fXy-during <fxy1044667399@163.com>
2021-02-04 17:11:47 +0800

    feat: cli调整

--- packages/feflow-cli/src/cli/index.ts
+++ packages/feflow-cli/src/cli/index.ts
@@ -23,7 +23,7 @@ const checkNodeVersion = (wanted: any, id: string) => {
           id +
           ' requires Node ' +
           wanted +
-          '.\nPlease upgrade your Node version。'
+          '.\nPlease upgrade your Node version..'
       )
     );
     process.exit(1);
