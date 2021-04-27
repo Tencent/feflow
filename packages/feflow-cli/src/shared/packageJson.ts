6748c02eed1c432fed6963b203fbe8585af45944
MrGaoGang <gaogangwork@qq.com>
2021-04-27 19:30:53 +0800

    feat(shared): 新增install 指定版本的插件

--- packages/feflow-cli/src/shared/packageJson.ts
+++ packages/feflow-cli/src/shared/packageJson.ts
@@ -4,17 +4,23 @@ export default function packageJson(
   name: string,
   registry: string
 ): Promise<string> {
+  const names = (name || '').split('@');
   return new Promise((resolve, reject) => {
     const options = {
-      url: `${registry}/${name}`,
+      url: `${registry}/${names[1] ? names.join('/') : names[0]}`,
       method: 'GET'
     };
 
     rp(options)
       .then((response: any) => {
         const data = JSON.parse(response);
-        const version = data['dist-tags'].latest;
-        resolve(version);
+        if (names.length === 1) {
+          const version = data['dist-tags'].latest;
+          resolve(version);
+        } else {
+          // 指定包的版本情况
+          resolve(data['version']);
+        }
       })
       .catch((err: object) => {
         reject(err);
