7f94e8955a31ca9701cab43c89794fd7d5644c86
guichezhang <guichezhang@tencent.com>
2022-02-27 23:42:44 +0800

    feat: 完成fef list测试
    --story=20220227

--- packages/feflow-cli/__tests__/core/native/list.test.ts
+++ packages/feflow-cli/__tests__/core/native/list.test.ts
@@ -4,7 +4,17 @@ import chai from 'chai';
 const { expect } = chai;
 
 describe('@feflow/core - Native list', () => {
-  it('测试 fef logger', () => {
-    expect(shell.exec(`node bin/feflow list --debug --e2e`).stdout).to.equal('nothing types\n');
+  afterEach(() => {
+    shell.exec('rm -rf .fef');
+  });
+
+  it('测试 fef list', () => {
+    // 安装 lodash.sum
+    const packageName = '@feflow/generator-example';
+    const edition = '0.0.7';
+    shell.exec(`node bin/feflow install ${packageName}@${edition} --e2e`);
+
+    const expectContent = `You can search more templates or plugins through https://feflowjs.com/encology/\ntemplates\n${packageName}(${edition})\nplugins\nNo plugins have been installed\n`;
+    expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
   });
 });
