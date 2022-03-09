8d8f28d33e1681d9c30cbbd86eb02523621f4b88
guichezhang <guichezhang@tencent.com>
2022-03-09 15:26:59 +0800

    feat: 完成fef install  命令测试
    --story=20220309

--- packages/feflow-cli/__tests__/core/native/install.test.ts
+++ packages/feflow-cli/__tests__/core/native/install.test.ts
@@ -8,8 +8,24 @@ describe('@feflow/core - Native list', () => {
     shell.exec('rm -rf .fef');
   });
 
-  it('测试 fef install 后面的参数为空', () => {
-    // 安装 @feflow/generator-example
+  it('fef install 后面的参数为空时返回的信息为空，并且 fef list 的结果中不包含脚手架或插件', () => {
+    expect(shell.exec(`node bin/feflow install --e2e`).stdout).to.equal('');
+
+    const expectContent = `You can search more templates or plugins through https://feflowjs.com/encology/\nNo templates and plugins have been installed\n`;
+    expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
+  });
+
+  it('fef install 安装不存在的脚手架时应返回Feflow提示信息，并且 fef list 的返回结果中不包含脚手架或插件', () => {
+    const packageName = 'pkg_123654';
+    expect(shell.exec(`node bin/feflow install ${packageName} --e2e`).stdout).to.equal(
+      `[ Feflow Info ][ native ] Installing packages. This might take a couple of minutes.\n`,
+    );
+
+    const expectContent = `You can search more templates or plugins through https://feflowjs.com/encology/\nNo templates and plugins have been installed\n`;
+    expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
+  });
+
+  it('fef install 安装脚手架时，fef list 的返回结果中应包含该脚手架', () => {
     const packageName = '@feflow/generator-example';
     const edition = '0.0.7';
     shell.exec(`node bin/feflow install ${packageName}@${edition} --e2e`);
@@ -18,23 +34,12 @@ describe('@feflow/core - Native list', () => {
     expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
   });
 
-  // it('测试 fef install 安装脚手架', () => {
-  //   // 安装 @feflow/generator-example
-  //   const packageName = '@feflow/generator-example';
-  //   const edition = '0.0.7';
-  //   shell.exec(`node bin/feflow install ${packageName}@${edition} --e2e`);
+  it.only('fef install 安装插件时，fef list 的返回结果中应包含该插件', () => {
+    const packageName = 'feflow-plugin-lint';
+    const edition = '1.1.3';
+    shell.exec(`node bin/feflow install ${packageName}@${edition} --e2e`);
 
-  //   const expectContent = `You can search more templates or plugins through https://feflowjs.com/encology/\ntemplates\n${packageName}(${edition})\nplugins\nNo plugins have been installed\n`;
-  //   expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
-  // });
-
-  // it('测试 fef install 安装插件', () => {
-  //   // 安装 @feflow/generator-example
-  //   const packageName = '@feflow/generator-example';
-  //   const edition = '0.0.7';
-  //   shell.exec(`node bin/feflow install ${packageName}@${edition} --e2e`);
-
-  //   const expectContent = `You can search more templates or plugins through https://feflowjs.com/encology/\ntemplates\n${packageName}(${edition})\nplugins\nNo plugins have been installed\n`;
-  //   expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
-  // });
+    const expectContent = `You can search more templates or plugins through https://feflowjs.com/encology/\ntemplates\nNo templates have been installed\nplugins\n${packageName}(${edition})\n`;
+    expect(shell.exec(`node bin/feflow list --e2e`).stdout).to.equal(expectContent);
+  });
 });
