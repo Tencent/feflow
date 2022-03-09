8b88caac5d8fdeea1649cdd6ff241e45d9fdd8de
guichezhang <guichezhang@tencent.com>
2022-03-09 22:32:12 +0800

    feat: 完成fef doctor端到端测试
    --story=20220309

--- packages/feflow-cli/__tests__/core/native/doctor.test.ts
+++ packages/feflow-cli/__tests__/core/native/doctor.test.ts
@@ -1,7 +1,7 @@
 import shell from 'shelljs';
-// import chai from 'chai';
+import chai from 'chai';
 
-// const { expect } = chai;
+const { assert } = chai;
 
 describe('@feflow/core - Native doctor', () => {
   beforeEach(() => {
@@ -9,6 +9,8 @@ describe('@feflow/core - Native doctor', () => {
   });
 
   it('fef doctor 的输出信息中应该包含 Tools Version 和 Proxy config info', () => {
-    shell.exec('node bin/feflow doctor --e2e');
+    // 由于每台机器上node等版本可能不同，因此这里不对完整的信息做断言，只检查是否包含关键信息来保证fef doctor正常运行即可
+    const pattern = /[\s\S]*Tools Version[\s\S]*Proxy config info[\s\S]*/;
+    assert(pattern.exec(shell.exec('node bin/feflow doctor --e2e').stdout));
   });
 });
