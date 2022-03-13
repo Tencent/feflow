c3a2aeb6be67e336ac3b58fdae966329eadd68b7
happyCoding1024 <zhangguicheng_1995@163.com>
2022-03-13 22:45:03 +0800

    feat: 测试node version
    --story=20220313

--- packages/feflow-cli/__tests__/core/native/info.test.ts
+++ packages/feflow-cli/__tests__/core/native/info.test.ts
@@ -11,6 +11,7 @@ describe('@feflow/core - Native info', () => {
   it.only('fef info 的输出中应该包含feflow以及它的版本信息', () => {
     // 因为每台机器上的node版本等信息可能不同，因此这里采用正则的方式不去断言全部的返回信息，只验证feflow这个字段保证fef info正常输出即可
     const pattern = /feflow: \d+.\d+.\d+[-]*[\w.\d]*/;
+    console.log('E2E: node version === ', shell.exec('node --version').stdout);
     console.log('E2E: fef info === ', shell.exec('node bin/feflow info --e2e').stdout);
     assert(pattern.test(shell.exec('node bin/feflow info --e2e').stdout));
   });
