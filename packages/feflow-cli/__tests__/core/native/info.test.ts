bbb02ce6c979fe1d6fc3b441e7c2f9c581ff1b9b
guichezhang <guichezhang@guichezhangdeMacBook-Pro.local>
2022-03-14 16:34:57 +0800

    feat: 验证feflow-cli目录
    --story=20220314

--- packages/feflow-cli/__tests__/core/native/info.test.ts
+++ packages/feflow-cli/__tests__/core/native/info.test.ts
@@ -8,7 +8,7 @@ describe('@feflow/core - Native info', () => {
     shell.exec('rm -rf .fef');
   });
 
-  it('fef info 的输出中应该包含feflow以及它的版本信息', () => {
+  it.only('fef info 的输出中应该包含feflow以及它的版本信息', () => {
     // 因为每台机器上的node版本等信息可能不同，因此这里采用正则的方式不去断言全部的返回信息，只验证feflow这个字段保证fef info正常输出即可
     const pattern = /feflow: \d+.\d+.\d+[-]*[\w.\d]*/;
     assert(pattern.test(shell.exec('node bin/feflow info --e2e').stdout));
