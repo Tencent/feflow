e24caa63c80a45eaf3d945ef9e802065cd8928c3
guichezhang <guichezhang@guichezhangdeiMac.local>
2022-03-26 23:13:34 +0800

    feat: 测试fef info
    --story=20220326

--- packages/feflow-cli/__tests__/core/native/info.test.ts
+++ packages/feflow-cli/__tests__/core/native/info.test.ts
@@ -1,8 +1,7 @@
 import shell from 'shelljs';
-// import chai from 'chai';
-import process from 'child_process';
+import chai from 'chai';
 
-// const { assert } = chai;
+const { assert } = chai;
 
 describe('@feflow/core - Native info', () => {
   beforeEach(() => {
@@ -11,18 +10,7 @@ describe('@feflow/core - Native info', () => {
 
   it.only('fef info 的输出中应该包含feflow以及它的版本信息', (done) => {
     // 因为每台机器上的node版本等信息可能不同，因此这里采用正则的方式不去断言全部的返回信息，只验证feflow这个字段保证fef info正常输出即可
-    // const pattern = /feflow: \d+.\d+.\d+[-]*[\w.\d]*/;
-    // console.log('E2E: node version === ', shell.exec('node --version').stdout);
-    // console.log('E2E: fef info === ', shell.exec('node bin/feflow info --e2e').stdout);
-    // assert(pattern.test(shell.exec('node bin/feflow info --e2e').stdout));
-
-    process.exec('node bin/feflow info --e2e', (error, stdout) => {
-      if (error !== null) {
-        console.log(`exec error: ${error}`);
-      }
-
-      console.log('E2E: stdout === ', stdout);
-      done();
-    });
+    const pattern = /feflow: \d+.\d+.\d+[-]*[\w.\d]*/;
+    assert(pattern.test(shell.exec('node bin/feflow info --e2e').stdout));
   });
 });
