78268ef000b8f3d24dab741525c36d17477828b0
happyCoding1024 <zhangguicheng_1995@163.com>
2022-03-27 22:21:05 +0800

    feat: 测试Linux和window上运行
    --story=20220327

--- packages/feflow-cli/__tests__/core/native/info.test.ts
+++ packages/feflow-cli/__tests__/core/native/info.test.ts
@@ -9,19 +9,10 @@ describe('@feflow/core - Native info', () => {
   });
 
   it.only('fef info 的输出中应该包含feflow以及它的版本信息', () => {
-    console.log('执行 source bash_profile');
     console.log(shell.exec('source ~/.bash_profile'));
-    console.log(shell.exec('. ~/.bash_profile'));
-    console.log(shell.exec('node bin/feflow info --e2e'));
-    console.log(shell.exec('source ~/.bash_profile'));
-    console.log(shell.exec('. ~/.bash_profile'));
-    console.log(shell.exec('cd ~ & ls -lah & cat ~/.bash_profile'));
     // 因为每台机器上的node版本等信息可能不同，因此这里采用正则的方式不去断言全部的返回信息，只验证feflow这个字段保证fef info正常输出即可
     const pattern = /feflow: \d+.\d+.\d+[-]*[\w.\d]*/;
     assert(pattern.test(shell.exec('node bin/feflow info --e2e').stdout));
-  });
-
-  it.only('fef info 输出信息中应该包含系统型号', () => {
-    console.log(shell.exec('node bin/feflow info --e2e'));
+    console.log(shell.exec('source ~/.bash_profile'));
   });
 });
