184c92670820c8ff9cfb6fc31f9792b585461974
guichezhang <guichezhang@tencent.com>
2022-05-13 23:49:48 +0800

    feat: 优化pm2管理心跳进程逻辑
    --story=20220513

--- packages/feflow-cli/src/shared/pm2.ts
+++ packages/feflow-cli/src/shared/pm2.ts
@@ -1,17 +1,21 @@
 import pm2 from 'pm2';
+import Feflow from '..';
+
+export type ErrProcCallback = (pm2: any) => (err: Error, proc: pm2.Proc) => void;
 
 /**
  * 创建pm2进程
  *
  * @param options pm2启动进程参数
  */
-export function createPm2Process(options: pm2.StartOptions) {
+export function createPm2Process(ctx: Feflow, options: pm2.StartOptions, errback: ErrProcCallback) {
   pm2.connect((err) => {
     if (err) {
-      console.error(err);
-      process.exit(2);
+      ctx.logger.error('pm2 connect failed', err);
+      return;
     }
+
     // To release connection to PM2 and make your application auto exit, make sure to disconnect from pm2 with pm2.disconnect()
-    pm2.start(options, () => pm2.disconnect());
+    pm2.start(options, errback(pm2));
   });
 }
