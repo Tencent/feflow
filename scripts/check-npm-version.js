84e0d871691ba65bcca9d89d38fdda2ad1b4fb8c
guichezhang <guichezhang@tencent.com>
2022-07-15 16:20:24 +0800

    feat: 测试git user
    --story=20220715

--- scripts/check-npm-version.js
+++ scripts/check-npm-version.js
@@ -7,19 +7,17 @@ if (process.env.CI) {
 }
 
 const restrictMajorVersion = '7';
-const semverRegExp =
-  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+const semverRegExp =  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 
 exec('npm --version', (error, stdout, stderr) => {
   if (error || !stdout) {
     console.error(error || stderr);
     process.exit(1);
   }
+
   const [, majorVersion] = semverRegExp.exec(stdout) || [];
   if (majorVersion >= restrictMajorVersion) {
-    console.log(
-      `Please use a version of npm lower than v${restrictMajorVersion} for development due to a bug of lerna: https://github.com/lerna/lerna/issues/2832 .`,
-    );
+    console.log(`Please use a version of npm lower than v${restrictMajorVersion} for development due to a bug of lerna: https://github.com/lerna/lerna/issues/2832 .`);
     process.exit(1);
   } else {
     console.log('npm version ok');
