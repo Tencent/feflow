368bc7fbf5d3e76e91c6ffa73dee0c290bd3d8b1
happyCoding1024 <zhangguicheng_1995@163.com>
2022-07-17 14:58:37 +0800

    feat: 使用rmdirSync代替rm删除文件夹
    --story=20220717

--- packages/feflow-cli/__tests__/core/native/install.test.ts
+++ packages/feflow-cli/__tests__/core/native/install.test.ts
@@ -1,16 +1,19 @@
 import shell from 'shelljs';
 import chai from 'chai';
+import fs from 'fs';
 
 const { assert } = chai;
 
 describe('@feflow/core - Native list', () => {
   beforeEach(() => {
-    shell.exec('rm -rf .fef');
+    fs.rmdirSync('.fef', {
+      recursive: true,
+    });
   });
 
   it('fef install 后面的参数为空时返回参数错误的提示信息', () => {
     const expectedPattern = /\[ Feflow Error \]\[ feflow-cli \] parameter error\n*/;
-    assert.match(shell.exec(`node bin/feflow install`).stderr, expectedPattern);
+    assert.match(shell.exec('node bin/feflow install').stderr, expectedPattern);
   });
 
   it('fef install 安装脚手架成功时,fef list 的返回信息中应包含该脚手架', () => {
@@ -18,9 +21,7 @@ describe('@feflow/core - Native list', () => {
     const edition = '0.0.7';
     shell.exec(`node bin/feflow install ${packageName}@${edition}`);
 
-    const expectedPattern = new RegExp(
-      `You can search more templates or plugins through https:\\/\\/feflowjs.com\\/encology\\/\\n*templates\\n*${packageName}\\(${edition}\\)\\n*plugins\\n*No plugins have been installed\\n*`,
-    );
-    assert.match(shell.exec(`node bin/feflow list`).stdout, expectedPattern);
+    const expectedPattern = new RegExp(`You can search more templates or plugins through https:\\/\\/feflowjs.com\\/encology\\/\\n*templates\\n*${packageName}\\(${edition}\\)\\n*plugins\\n*No plugins have been installed\\n*`);
+    assert.match(shell.exec('node bin/feflow list').stdout, expectedPattern);
   });
 });
--- packages/feflow-cli/__tests__/core/native/list.test.ts
+++ packages/feflow-cli/__tests__/core/native/list.test.ts
@@ -1,16 +1,19 @@
 import shell from 'shelljs';
 import chai from 'chai';
+import fs from 'fs';
 
 const { assert } = chai;
 
 describe('@feflow/core - Native list', () => {
   beforeEach(() => {
-    shell.exec('rm -rf .fef');
+    fs.rmdirSync('.fef', {
+      recursive: true,
+    });
   });
 
   it('未安装任何脚手架或插件时, fef list 应提示未安装任何脚手架或插件', () => {
     const expectedPattern = /No templates and plugins have been installed/;
-    assert.match(shell.exec(`node bin/feflow list`).stdout, expectedPattern);
+    assert.match(shell.exec('node bin/feflow list').stdout, expectedPattern);
   });
 
   it('安装 @feflow/generator-example 脚手架后, fef list 的返回内容应包含 @feflow/generator-example', () => {
@@ -20,6 +23,6 @@ describe('@feflow/core - Native list', () => {
     shell.exec(`node bin/feflow install ${packageName}@${edition}`);
 
     const expectedPattern = new RegExp(`templates\\n*${packageName}\\(${edition}\\)\\n*plugins\\n*`);
-    assert.match(shell.exec(`node bin/feflow list`).stdout, expectedPattern);
+    assert.match(shell.exec('node bin/feflow list').stdout, expectedPattern);
   });
 });
