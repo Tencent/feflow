192cbfdc8f363c262191d77ab3183485963d5ee4
guichezhang <guichezhang@tencent.com>
2022-10-24 21:52:18 +0800

    feat: fef info 断言精确到版本
    --story=20221024

--- packages/feflow-cli/__tests__/core/native/info.test.ts
+++ packages/feflow-cli/__tests__/core/native/info.test.ts
@@ -1,12 +1,16 @@
+import path from 'path';
 import shell from 'shelljs';
 import chai from 'chai';
+import { getKeyFormFile } from '../../../src/shared/file';
 
 const { assert } = chai;
 
 describe('@feflow/core - Native info', () => {
   it('fef info 的输出中应该包含feflow以及它的版本信息', () => {
     // 因为每台机器上的node版本等信息可能不同，因此这里采用正则的方式不去断言全部的返回信息，只验证feflow这个字段保证fef info正常输出即可
-    const expectedPattern = /feflow: \d+.\d+.\d+[-]*[\w.\d]*/;
+    const packageJsonPath = path.resolve(__dirname, '../../../package.json');
+    const feflowVersion = getKeyFormFile(packageJsonPath, 'version');
+    const expectedPattern = new RegExp(`feflow: ${feflowVersion}`);
     assert.match(shell.exec('node bin/feflow info').stdout, expectedPattern);
   });
 });
