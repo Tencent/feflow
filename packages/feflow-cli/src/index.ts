152498085d88db9558ba53e110947ede23593267
alexqxxu <alexqxxu@tencent.com>
2023-03-29 16:25:00 +0800

    feat(@feflow/cli): 优化顶层导出，便于进行 ts module augmentation

--- packages/feflow-cli/src/index.ts
+++ packages/feflow-cli/src/index.ts
@@ -1,2 +1,7 @@
 import Feflow from './core';
+
+// 命名导出便于插件开发者进行 ts module augmentation: https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+// 因为 default 导出不能被 augment
+export { Feflow };
+// legacy default 导出，后续不建议
 export default Feflow;
